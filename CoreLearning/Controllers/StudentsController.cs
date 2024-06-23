c1e773418764984b9904c51fe2f0f4ccfe8030ec
psgatwork <psgatwork@gmail.com>
2024-06-23 11:23:02 +0530

    Added Student Controller, a new endpoint to the api.

---
+++ CoreLearning/Controllers/StudentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreLearning.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "ගයාන්", "ජානබ", "ව්කල්ස්", "ලෙක්ස්" };  
+            return Ok(studentNames);
+        }
+    }
+}
