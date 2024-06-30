9de65b27c383014be365b9f8aa7cd268e2f5585d
psgatwork <psgatwork@gmail.com>
2024-06-30 13:36:51 +0530

    Added Migration

--- CoreLearning/Program.cs
+++ CoreLearning/Program.cs
@@ -1,4 +1,7 @@
 
+using CoreLearning.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace CoreLearning
 {
     public class Program
@@ -13,6 +16,9 @@ namespace CoreLearning
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<NZWalksDbContext>(options => 
+            options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnectionString")));
 
             var app = builder.Build();
 
