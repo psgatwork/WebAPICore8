4370b65169dcaa0df99a6dc62cc6b90ed361c213
psgatwork <psgatwork@gmail.com>
2024-06-30 12:47:56 +0530

    Added DB Context class

---
+++ CoreLearning/Data/NZWalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CoreLearning.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreLearning.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
